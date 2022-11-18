7cedfd98a71898405ca4d739ceedaac99146f2ea
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-11-17 21:24:18 -0300

    Commit - Comentarios JS

--- _js/comentario.js
+++ _js/comentario.js
@@ -6,7 +6,14 @@ const commentPost = document.getElementById('commentPost');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let p = document.createElement('p');
-    p.classList = 'p-2 d-flex text-wrap'
-    p.innerHTML = `<strong>${inputName.value}: </strong> ${textComment.value}`;
-    
+    p.classList = 'p-2 d-flex text-wrap text-snow flex-wrap'
+    p.innerHTML = `<strong>${inputName.value}:</strong>&nbsp${textComment.value}`;
+    commentPost.appendChild(p);
+    esvaziarForm();
+    inputName.focus();
 });
+
+function esvaziarForm() {
+    inputName.value = '';
+    textComment.value = '';
+}
